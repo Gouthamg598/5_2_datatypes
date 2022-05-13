ffb96610f21a896ed38e0eed5d57a2edf8913475
Goutham <104981580+Gouthamg598@users.noreply.github.com>
2022-05-13 12:31:38 +0530

    Add files via upload

---
+++ datatypes_01.py
@@ -0,0 +1,53 @@
+'''name:gouotham
+date:12-05-2022
+file discription:string concatenation
+'''
+#we can join two or more strings together
+# #we can not concatenate int and float with string
+# from curses.ascii import isspace
+
+
+# a='string1';c=6
+# b='string2';d=6.5
+# # print((a+b)*2)
+# print(type(a))
+# print(type(b))
+# print(a+c)TypeError: can only concatenate str (not "int") to str
+# print(b+d)TypeError: can only concatenate str (not "float") to str
+# print(a,c)#string1 6
+# print(b,c)#string2 6
+# print(d,b)#6.5 string2
+# print(a,b)
+# print('')# empty string
+# print(type(''))#string
+'''
+"".isspace() is give the space are there in string or not in the form of True or False'''
+# print(''.isspace()) #False
+# print('b'.isspace()) #False
+# print('  '.isspace()) #True
+# print('     '.isspace()) #True
+'''for space  we use , and '_' '''
+
+
+# print('h'   'i') hi
+# print('h '   'i')h i
+# print('h'   ' i')h i
+# print('h','i')h i
+# print('h'+'i')hi
+# print('h'+'  '+'i')h i
+# print('py'+'  '+'3')#py   3
+# print('py',3)#,it give single space
+# print('python '+'3.4.10')
+# print('python','3.4.10')
+# print('python'+' '+'3.4.10')
+# print('TATA'+'-'+'IPL')
+
+# a=5.6;b=6.8;c='pyhton'
+# print('%f %d' %(a,b))
+# print('%f %f' %(a,b))
+# print('%s %i'%(c,a))
+# print(c,a)
+# p='python';r=3;c=10
+# print(p+'-'+str(r))
+# print('python '+str(3)+' and'+' fullstack')
+
